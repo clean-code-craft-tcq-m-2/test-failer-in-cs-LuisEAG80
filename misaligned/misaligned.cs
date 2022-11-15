2333ca9e8416cfd4199ebce64259db3557f6a17c
Luis Acosta <luisenriqueacosta@gmail.com>
2022-11-15 00:13:31 -0600

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,22 +1,33 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
 namespace MisalignedSpace {
     class Misaligned {
-        static int printColorMap() {
+        static string[] printColorMap() {
             string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
             string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
+            string[] colorMapRows = new string[majorColors.Length * minorColors.Length];
+
             int i = 0, j = 0;
             for(i = 0; i < 5; i++) {
                 for(j = 0; j < 5; j++) {
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+                    colorMapRows[i * 5 + j] = string.Format("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[j]);
                 }
             }
-            return i * j;
+
+            Console.WriteLine("{0}", string.Join(Environment.NewLine, colorMapRows));
+            return colorMapRows;
         }
+
         static void Main(string[] args) {
-            int result = printColorMap();
-            Debug.Assert(result == 25);
+            string[] result = printColorMap();
+            Debug.Assert(result.Length == 25);
+
+            foreach (var row in result)
+            {
+                Debug.Assert(row.IndexOf("|") == 2);
+            }
+
             Console.WriteLine("All is well (maybe!)");
         }
     }
